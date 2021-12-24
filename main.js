bb99009596c4eac381ad6c27a5c5d0a95bce890c
PrinceCodes <princecodes247@gmail.com>
2021-12-24 20:24:53 +0100

    feat: add js

---
+++ main.js
@@ -0,0 +1,29 @@
+let header = document.querySelector("header")
+
+header.classList.add("small")
+let body = document.querySelector("body");
+// document.addEventListener("scroll", e => {
+// 	console.log(e)
+// })
+
+let highlights = {
+	today: {
+		name: "Chohe Pancakes",
+		sides: "Chicken Wings",
+		topping: "Pepper Sauce",
+	},
+	week: {
+		name: "Sapa Pancakes",
+		sides: "Chicken Wings",
+		topping: "Pepper Sauce",
+	},
+	today: {
+		name: "Zazu Pancakes",
+		sides: "Chicken Wings",
+		topping: "Zeh Sauce",
+	},
+}
+
+let highlightTemplate = () => {
+	let a = ""
+}
