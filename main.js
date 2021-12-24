68a3b196abf41eea7d0dff719001d0ac46787dc1
PrinceCodes <princecodes247@gmail.com>
2021-12-24 21:34:59 +0100

    feat: update header nav effects and highlights

--- main.js
+++ main.js
@@ -1,29 +1,94 @@
+
+// For Header
 let header = document.querySelector("header")
 
-header.classList.add("small")
-let body = document.querySelector("body");
-// document.addEventListener("scroll", e => {
-// 	console.log(e)
-// })
+  
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    header.classList.add("small")
+  } else {
+    header.classList.remove("small")
+  }
+} 
+
+// For highlights nav
+let highlightNavs = document.querySelectorAll(".highlights_nav .nav_item")
+
+highlightNavs.forEach(item => {
+	item.addEventListener("click", () => {
+		highlightNavs.forEach(targ => {
+			targ.classList.remove("nav_item-active")
+		})
+		item.classList.add("nav_item-active")
+
+	})
+})
+
+
+// For highlights
 let highlights = {
-	today: {
+	today: [
+		{
 		name: "Chohe Pancakes",
 		sides: "Chicken Wings",
-		topping: "Pepper Sauce",
-	},
-	week: {
+		toppings: "Pepper Sauce",
+		price: "5000"
+	}
+	],
+	week: [
+	{
 		name: "Sapa Pancakes",
 		sides: "Chicken Wings",
-		topping: "Pepper Sauce",
-	},
-	today: {
+		toppings: "Pepper Sauce",
+		price: "5000"
+	}
+	],
+	month: [
+	{
 		name: "Zazu Pancakes",
 		sides: "Chicken Wings",
-		topping: "Zeh Sauce",
-	},
+		toppings: "Zeh Sauce",
+		price: "5000"
+	}
+	],
 }
 
-let highlightTemplate = () => {
-	let a = ""
+let createHighlight = (data, target) => {
+	let title = document.createElement("h3")
+	title.innerText = data.name
+		let sides = document.createElement("p")
+	sides.innerText = data.sides
+	let toppings = document.createElement("p")
+	toppings.innerText = data.toppings
+	let price = document.createElement("p")
+	price.innerText = "N" + data.price
+	let order = document.createElement("button")
+	order.innerText = "Place your Order"
+	
+	let info = document.createElement("div")
+	info.classList.add("order_info")
+	info.appendChild(title)
+	info.appendChild(sides)
+	info.appendChild(toppings)
+	info.appendChild(price)
+	info.appendChild(order)
+	let img = document.createElement("div")
+	img.classList.add("order_img")
+
+		let body = document.createElement("div")
+	body.classList.add("order")
+	body.appendChild(img)
+	body.appendChild(info)	
+	target.appendChild(body)	
 }
+
+
+let highlightsSlides = document.querySelector(".highlights_slides")
+
+highlights.today.forEach(item => {
+	createHighlight(item, highlightsSlides)
+	
+})
